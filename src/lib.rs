c7added2bd7630df67112ff7c9f3c1b832fd798b
Louis-Tarvin <louis.tarvin@gmail.com>
2019-09-16 17:53:22 +0100

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
+mod compiler;
 mod instruction;
 mod virtual_machine;
-mod compiler;
 
+pub use compiler::*;
 pub use instruction::*;
 pub use virtual_machine::*;
-pub use compiler::*;
--- tests/compilation.rs
+++ tests/compilation.rs
@@ -1,9 +1,9 @@
-use darwin_lib::{parse_program, create_program};
+use darwin_lib::{create_program, parse_program};
 
 #[test]
 fn simple_program() {
     assert_eq!(
         parse_program("MOV 0 1").unwrap(),
-        create_program!{ MOV(I, 0, Direct, 1, Direct) },
+        create_program! { MOV(I, 0, Direct, 1, Direct) },
     );
 }
